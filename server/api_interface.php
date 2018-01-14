c1b5b60af3d770c81c36a07f31f039d47c9fc744
developer <lightmorning46@gmail.com>
2018-01-15 03:04:49 +0800

    api updated

--- server/api_interface.php
+++ server/api_interface.php
@@ -46,19 +46,42 @@ if (isset($_REQUEST)) {
 		    	$ret = array('status_code' => 400, 'count' => 0, 'data' => []);
 		    break;
         case 'get_drug_property':
-        	$ndc = $_REQUEST['ndc'];
-			$sql = "SELECT Drug_Properties.*, patient_interactivity_code.prescription_ready FROM patient_interactivity_code LEFT JOIN Drug_Properties ON patient_interactivity_code.ndc1=Drug_Properties.ndc WHERE patient_interactivity_code.ndc1='$ndc'";
+        	$code = $_REQUEST['code'];
+			$sql = "SELECT * FROM patient_interactivity_code WHERE interactive_code='$code'";
 		    $data = array();
 		    if ($result = $mysqli->query($sql)) {
 				if ($result->num_rows>0)
 				{
-					$ret['status_code'] = 200;
-					$ret['count'] = $result->num_rows;
-					while($row = $result->fetch_assoc())
+					$row = $result->fetch_assoc();
+					$sql1 = "SELECT * from Drug_Properties where ";
+					for ($i=1;$i<11;$i++)
 					{
-						$data[] = $row;
+						$ndc = $row["ndc{$i}"];
+						if ($ndc != "" && $ndc != null)
+						{
+							$sql1 .= " ndc='$ndc' OR ";
+						}
 					}
-					$ret['data'] = $data;
+					$sql1 = substr($sql1, 0, strlen($sql1)-3);
+					if ($result1 = $mysqli->query($sql1))
+					{
+						if ($result1->num_rows > 0)
+						{
+							$ret['status_code'] = 200;
+							$ret['count'] = $result1->num_rows;
+							while($row1 = $result1->fetch_assoc())
+							{
+								$key = array_search($row1['ndc'], $row);
+								if (strlen($key) > 0){
+									$row1['status'] = $row['ndc' . substr($key,3) . '_status'];
+								}
+								else
+									$row1['status'] = "In Progress";
+								$data[] = $row1;
+							}
+							$ret['data'] = $data;
+						}
+					}
 				}
 				else
 					$ret = array('status_code' => 200, 'count' => 0, 'data' => []);
@@ -68,7 +91,7 @@ if (isset($_REQUEST)) {
 		    break;
         case 'drug_information':
         	$ndc = $_REQUEST['ndc'];
-			$sql = "SELECT patient_drug_information.* FROM patient_drug_information LEFT JOIN Drug_Properties ON patient_drug_information.brand_name=Drug_Properties.brand_name  WHERE Drug_Properties.ndc='$ndc'";
+			$sql = "SELECT patient_drug_information.* FROM patient_drug_information LEFT JOIN Drug_Properties ON patient_drug_information.generic_name=Drug_Properties.generic_name  WHERE Drug_Properties.ndc='$ndc'";
 		    $data = array();
 		    if ($result = $mysqli->query($sql)) {
 				if ($result->num_rows>0)
@@ -170,22 +193,44 @@ if (isset($_REQUEST)) {
 		    	$ret = array('status_code' => 400, 'count' => 0, 'data' => []);
 		    break;
 		case 'quiz_save':
-        	$ndc = $_REQUEST['ndc'];
-        	$sql = "";
+        	$code = $_REQUEST['code'];
+        	$count = $_REQUEST['count'];
+        	$sql = "INSERT INTO quiz_user_answer (quiz_question_id, quiz_user_answer, interactive_code) VALUES ";
+        	for ($i=0;$i<$count;$i++)
+        	{
+        		$qid = $_REQUEST['qid'.($i+1)];
+        		$answer = $mysqli->escape_string($_REQUEST['answer'.($i+1)]);
+        		$sql .= "('{$qid}','{$answer}','$code')";
+        		if ($i != $count-1) $sql .= ", ";
+        	}
+        	
+		    $data = array();
+		    if ($result = $mysqli->query($sql)) {
+				$ret = array('status_code' => 200, 'data' => 'success');
+		    }
+		    else
+		    	$ret = array('status_code' => 400, 'data' => 'failed to save');
+        	break;
+        case 'other';
+        	$field = $_REQUEST['field'];
+			$sql = "SELECT * FROM terms_of_use LIMIT 1";
 		    $data = array();
 		    if ($result = $mysqli->query($sql)) {
 				if ($result->num_rows>0)
 				{
-					
+					$ret['status_code'] = 200;
+					$row = $result->fetch_assoc();
+					$data = $row[$field];
+					$ret['data'] = $data;
 				}
 				else
-					$ret = array('status_code' => 200, 'count' => 0, 'data' => []);
+					$ret = array('status_code' => 200, 'data' => "");
 		    }
 		    else
-		    	$ret = array('status_code' => 400, 'count' => 0, 'data' => []);
-        	break;
+		    	$ret = array('status_code' => 400, 'data' => "");
+		    break;
         default:
-        	$ret = array('status_code' => 400, 'data' => []);
+        	$ret = array('status_code' => 400, 'data' => "");
         	break;
     }
     echo json_encode($ret);
